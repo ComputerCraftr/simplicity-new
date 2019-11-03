46394971543504b8d32de24535e59fdc84bd381b
John <ComputerCraftr@users.noreply.github.com>
2019-11-03 03:10:08 -0700

    GUI update

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -314,7 +314,7 @@ public:
 
         // Stake Settings
         nHashDrift = 45;
-        nStakeSplitThreshold = 2000;
+        nStakeSplitThreshold = 999999;
         nHashInterval = 22;
         nStakeSetUpdateTime = 300; // 5 minutes
 
